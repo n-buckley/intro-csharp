3cf961352710ad3cc2f7052c11c0f613c6f19f12
n-buckley <buckley.ni@northeastern.edu>
2024-06-19 16:18:41 -0400

    read, write, convert, concat

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var age = 23; // automatically initialized as an Int
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
 
-//var big = 90000000; // int
-var big = 900000L; // int64
+Console.Write("What id your age: ");
+string ageInput = Console.ReadLine();
+int age = Convert.ToInt32(ageInput);
 
-// not really great for readablity (why does it exisit)
-
-const int gravity = -9;
-// gravity = 6 // illegal
-Console.WriteLine(gravity);
-
-const int vat = 20;
-const double perVAT = vat / 100D; //make sure to include D for good double division
-
-
-Console.ReadLine();
+Console.WriteLine("Your name is " + name + " and your age is " + age);
