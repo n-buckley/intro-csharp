21dc4a20ab5774ddb7b1e567331c71295f93bc2d
n-buckley <buckley.ni@northeastern.edu>
2024-06-19 15:45:45 -0400

    numeric data types

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int age = 23;
+Console.WriteLine(age);
+
+long bigNumber = 900000000L;
+Console.WriteLine(bigNumber);
+
+double negative = -55.22D; //D not necessary for double
+Console.WriteLine(negative);
+
+double precision = 5.000001F;
+Console.WriteLine(precision);
+
+decimal money = 14.99M; //decimal is different than double, even bigger than long 
+Console.WriteLine(money);
+
+int x = 10, 
+    y = 20, 
+    z = 30;
 
 Console.ReadLine();
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6898c5ca2aed733663d8c8ac26d8ec1cb89fa30")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
