892158892259ff5ed47358c7e5c8e845efe8baa3
n-buckley <buckley.ni@northeastern.edu>
2024-06-19 16:30:46 -0400

    if statements

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.Intrinsics.Arm;
+
 Console.Write("Enter your name: ");
 string name = Console.ReadLine();
 
@@ -8,3 +10,34 @@ string ageInput = Console.ReadLine();
 int age = Convert.ToInt32(ageInput);
 
 Console.WriteLine("Your name is " + name + " and your age is " + age);
+
+// == > >= < <= ! || &&
+if (age < 0 || age > 150){
+    Console.WriteLine("Invalid age");
+}
+else {
+    if (age >= 18 && age < 25){
+        Console.WriteLine("You are 18 or older");
+    }
+    else if (age >= 25) {
+        Console.WriteLine("You are 25 or older");
+
+    }
+}
+
+Console.Write("Enter a number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter another number: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("What is " + numA + " times " + numB + ": ");
+int answerInput = Convert.ToInt32(Console.ReadLine());
+
+int answer = numA * numB;
+if (answer == answerInput) {
+    Console.WriteLine("Correct");
+}
+else {
+    Console.WriteLine("No");
+}
