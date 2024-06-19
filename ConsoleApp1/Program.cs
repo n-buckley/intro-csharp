31caf99e7b0d31165c40e4f6200ee204cdabd7ec
n-buckley <buckley.ni@northeastern.edu>
2024-06-19 17:11:26 -0400

    FizzBuzz

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/* Create a for loop 1 to X (15)
+* 3 and 5 = FizzBuzz 
+* 3 = Fizz, 5 = Buzz (if divisible)
+* else = itself
+*/
 
-using System.Globalization;
-
-double value = 1000D / 12.34D;
-Console.WriteLine(value); // ugly 
-
-Console.WriteLine(string.Format("{0}, {1}",value, 1000)); // still ugly
-
-Console.WriteLine(string.Format("{0:0.00}", value)); //pretty, will round
-Console.WriteLine(string.Format("{0:0.00}", value)); //pretty, will round
-Console.WriteLine(string.Format("{0:0.0}", value)); //pretty, will round
-Console.WriteLine(string.Format("{0:0.#}", value)); //gets rid of trailing zero
-
-double money = 10D / 3D; 
-Console.WriteLine(string.Format("${0:0.00}", money)); //works for positive numbers
-money *= -1D;
-Console.WriteLine(string.Format("${0:0.00}", money)); //not for negative (negative sign is after $ sign, should be before)
-Console.WriteLine(money.ToString("C")); //C = currency
-Console.WriteLine(money.ToString("C0")); //C0 = no decimal
-Console.WriteLine(money.ToString("C1")); //C1 = 1 decimal
-Console.WriteLine(money.ToString("C2")); //C2 = 2 decimal
-
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-
+for (int i = 1; i <=15; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0 ){
+        Console.WriteLine("Fizz");
+    }
+    else if ( i % 5 == 0){
+        Console.WriteLine("Buzz");
+    }
+    else {
+        Console.WriteLine(i);
+    }
+}
