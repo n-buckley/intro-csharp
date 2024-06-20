54bad6498bbba452b7233b1c52523a651fdebe74
n-buckley <buckley.ni@northeastern.edu>
2024-06-19 20:27:52 -0400

    lists

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] angles = new int[3]; //makes array of size 3
-
-angles[0] = 0;
-angles[1] = 1; 
-angles[2] = 2;
-
-//angles.Length
-
-foreach (int n in angles){
-
-    Console.Write(n + " ");
-}
-Console.WriteLine();
-
 int[] numbers = new int[] 
 {
  9, 8, 7, 6, 5, 4, 3, 2, 1
 };
 
-Array.Sort(numbers); //mutates -- i forget the term...
-Console.WriteLine(string.Join(",", numbers));
+List<int> listEmpty = new List<int>();
+List<int> listDefault = new List<int>(3);
+List<int> listColl = new List<int>(); //Can also add another collection
+List<int> listNums = new List<int>()
+{
+    1, 2, 3, 4
+};
 
-Array.Reverse(numbers);
-Console.WriteLine(string.Join(",", numbers));
 
-// Array.Clear(numbers); //sets all values to default of the data type
-Array.Clear(numbers, 5, 4); // clears values starting at index 5 + 4
-Console.WriteLine(string.Join(",", numbers));
+listNums.Add(1);
+listNums.Add(2);
+listNums.Add(3);
+listNums.Add(4);
 
-Console.WriteLine(Array.IndexOf(numbers, 0)); //can also substring , start index, last index 
+for(int i =0; i < listNums.Count; i++){
+    Console.Write(listNums[i] + ", ");
+}
+Console.WriteLine();
+
+Console.WriteLine(string.Join(", ", listNums));
+
