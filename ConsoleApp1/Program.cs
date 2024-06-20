b08f29789cd45492be789552cdb47fe10796c174
n-buckley <buckley.ni@northeastern.edu>
2024-06-19 23:27:13 -0400

    class properties !!!(private this.name is modiefied by public this.Name)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,9 @@
 // how do we define class variables for THIS class?
 // https://youtu.be/YrtFtdTTfv0?si=TZTAaLvlkKQxzn9o&t=16940
 
+// in all your class fucntions you also want to use the Properties (whyy???) 
+// youre within the scope of the private variables why not just use them?
+
 //main:
 using System.Reflection.Metadata.Ecma335;
 
@@ -16,48 +19,58 @@ person.PrintDetails(); //object.Method()
 
 // no longer allowed with private fields
 
+//using getters and setters
 person.setName("Henry");
 Console.WriteLine($"{person.getName()} ");
 
+//using properties
+person.Name = "Gerald";
+Console.WriteLine($"{person.Name} ");
+
 class Person
 {
-    private string Name; 
-    private int Age;
-    private int BirthMonth;
+    private string name; 
+    private int age;
+    private int birthMonth; // i lied fields are REGULAR camel Case
+
+    public string Name
+    {
+        get => this.name;
+        set => this.name = value;
+    }
+
+    public string UserName {get; set;}
+    // just creating a property C will create a matching private variable 
+    // with default getter and setter 
+    // so you dont need to initialize a userName seperatley
 
     //contructor
     public Person(string name, int age, int birthMonth)
     {
         this.Name = name;
         //convention is also to make the fields Capital Camel
-        Age = age;
-        BirthMonth = birthMonth;
+        age = age;
+        birthMonth = birthMonth;
 
     }
 
     //also a constructor
     public Person(){}
 
+    // in all your other class fucntions you also want to use the Properties (whyy???)
+
     public string PrintDetails(){
-        Console.WriteLine($"Name: {Name}");
-        Console.WriteLine($"Age: {Age}");
-        Console.WriteLine($"Birth Month: {BirthMonth}");
+        Console.WriteLine($"Name: {Name}");  // using the property ***
+        Console.WriteLine($"Age: {this.age}");
+        Console.WriteLine($"Birth Month: {this.birthMonth}");
 
         return "success";
     }
 
-    public void setName(string name)
-    {
-        //check fo validity here 
-        Name = name;
-    }
-
-    public string getName()
-    {
-        return Name;
-    }
-
-    public int GetAge() => Age; //shorthand
+    // dont actually use these 
+    public void setName(string name) => this.name = name;
+    public string getName() => this.name;
+    public int GetAge() => this.age; //shorthand
 }
 
 
